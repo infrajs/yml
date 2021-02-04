cfae2b7ff41660559f1c645189e7d4843661c587
Anton Kiyatkin <a.kiyatkin@itlf.ru>
2021-02-04 16:02:26 +0400

    fix

--- Yml.php
+++ Yml.php
@@ -102,6 +102,7 @@ class Yml
 			
 			$poss[$k]['id'] = ++$pid;
 			$poss[$k]['categoryId'] = $groups[$pos['group_nick']]['id'];
+			if (isset($pos['images']))
 			foreach ($pos['images'] as $j => $v) {
 				$src = Path::theme($pos['images'][$j]);
 				$p = explode('/', $src);
